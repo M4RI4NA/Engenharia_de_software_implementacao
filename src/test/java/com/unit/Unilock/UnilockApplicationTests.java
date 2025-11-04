28620bc3927666ff50b1474d41545106b4960d91
1241159235 <abraao.luiz@souunit.com.br>
2025-11-04 19:41:24 -0300

    inicio

---
+++ src/test/java/com/unit/Unilock/UnilockApplicationTests.java
@@ -0,0 +1,13 @@
+package com.unit.Unilock;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UnilockApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
